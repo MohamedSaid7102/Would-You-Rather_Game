70bcb04847797a21172530fde920a5f92b030d55
Mohamed Shelf <mohamedsaidlearn@gmail.com>
2021-08-04 21:18:28 +0200

    Intial Commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+  render() {
+    return (
+      <header className="to-do-header">
+        Mo To-Do
+      </header>
+    )
+  }
+}
+
+export default Header
+
